a0e849e3c73263319aacd29e948682f72fbc00b3
Liu <josong04@gmail.com>
2016-10-24 13:00:25 +0900

    初めてのCommit

---
+++ main.cpp
@@ -0,0 +1,229 @@
+//=============================================================================
+//
+// メイン処理 [main.cpp]
+// Author : AKIRA TANAKA
+//
+//=============================================================================
+#include "main.h"
+#include "renderer.h"
+#include "scene.h"
+#include "scene2D.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define CLASS_NAME		"AppClass"			// ウインドウのクラス名
+#define WINDOW_NAME		"ポリゴンの描画"	// ウインドウのキャプション名
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//*****************************************************************************
+// グローバル変数:
+//*****************************************************************************
+CRenderer *g_pRenderer = NULL;
+CScene *g_pScene = NULL;
+
+#ifdef _DEBUG
+int		g_nCountFPS;			// FPSカウンタ
+#endif
+
+//=============================================================================
+// メイン関数
+//=============================================================================
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wcex =
+	{
+		sizeof(WNDCLASSEX),
+		CS_CLASSDC,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		NULL,
+		LoadCursor(NULL, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		NULL,
+		CLASS_NAME,
+		NULL
+	};
+	RECT rect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+	HWND hWnd;
+	MSG msg;
+	DWORD dwCurrentTime;
+	DWORD dwFrameCount;
+	DWORD dwExecLastTime;
+	DWORD dwFPSLastTime;
+	
+	// ウィンドウクラスの登録
+	RegisterClassEx(&wcex);
+
+	// 指定したクライアント領域を確保するために必要なウィンドウ座標を計算
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(CLASS_NAME,
+						WINDOW_NAME,
+						WS_OVERLAPPEDWINDOW,
+						CW_USEDEFAULT,
+						CW_USEDEFAULT,
+						(rect.right - rect.left),
+						(rect.bottom - rect.top),
+						NULL,
+						NULL,
+						hInstance,
+						NULL);
+
+	// レンダラーの生成
+	g_pRenderer = new CRenderer;
+	g_pRenderer->Init(hWnd, true);
+
+
+	//オブジェクトの生成
+	g_pScene = new CScene2D;
+	g_pScene->Init();
+
+	// 分解能を設定
+	timeBeginPeriod(1);
+
+	// フレームカウント初期化
+	dwCurrentTime =
+	dwFrameCount = 0;
+	dwExecLastTime = 
+	dwFPSLastTime = timeGetTime();
+
+	// ウインドウの表示
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	
+	// メッセージループ
+	while(1)
+	{
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+			{// PostQuitMessage()が呼ばれたらループ終了
+				break;
+			}
+			else
+			{
+				// メッセージの翻訳とディスパッチ
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+        }
+		else
+		{
+			dwCurrentTime = timeGetTime();
+			if((dwCurrentTime - dwFPSLastTime) >= 500)	// 0.5秒ごとに実行
+			{
+#ifdef _DEBUG
+				g_nCountFPS = dwFrameCount * 1000 / (dwCurrentTime - dwFPSLastTime);
+#endif
+				dwFPSLastTime = dwCurrentTime;
+				dwFrameCount = 0;
+			}
+
+			if((dwCurrentTime - dwExecLastTime) >= (1000 / 60))
+			{
+				dwExecLastTime = dwCurrentTime;
+
+				// レンダラーの更新処理
+				g_pRenderer->Update();
+
+				// レンダラーの描画処理
+				g_pRenderer->Draw();
+
+				dwFrameCount++;
+			}
+		}
+	}
+
+	//オブジェクトの破棄
+	if(g_pScene != NULL)
+	{
+		g_pScene->Uninit();
+		delete g_pScene;
+		g_pScene = NULL;
+	}
+
+	// レンダラーの破棄
+	if( g_pRenderer != NULL)
+	{
+		g_pRenderer->Uninit();
+		delete g_pRenderer;
+		g_pRenderer = NULL;
+	}
+	
+	
+
+
+	// ウィンドウクラスの登録を解除
+	UnregisterClass(CLASS_NAME, wcex.hInstance);
+
+	// 分解能を戻す
+	timeEndPeriod(1);
+
+	return (int)msg.wParam;
+}
+
+//=============================================================================
+// プロシージャ
+//=============================================================================
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+	case WM_CREATE:
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		switch(wParam)
+		{
+		case VK_ESCAPE:					// [ESC]キーが押された
+			DestroyWindow(hWnd);		// ウィンドウを破棄するよう指示する
+			break;
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+//=============================================================================
+// FPSを渡す関数
+//=============================================================================
+#ifdef _DEBUG
+
+int GetFPS(void)
+{
+	return g_nCountFPS;
+}
+
+#endif
+
+//=============================================================================
+// g_pRendererを渡す関数
+//=============================================================================
+CRenderer *GetRenderer(void)
+{
+	return g_pRenderer;
+}
+
+//=============================================================================
+// g_pSceneを渡す関数
+//=============================================================================
+CScene *GetScene(void)
+{
+	return g_pScene;
+}
---
+++ renderer.h
@@ -0,0 +1,44 @@
+//============================================
+//
+// タイトル:	 CL25課題
+// プログラム名: renderer.h
+// 作成者:		 HAL東京ゲーム学科　劉南宏
+// 作成日:       2016/10/19
+//
+//============================================
+
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+
+//============================================
+//マクロ定義
+//============================================
+
+//============================================
+//構造体定義
+//============================================
+//敵クラス
+class CRenderer
+{
+public:
+
+	CRenderer();
+	virtual ~CRenderer();
+
+	LPDIRECT3DDEVICE9 GetDevice();
+
+	HRESULT Init(HWND hWnd, bool bWindow);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+private:
+
+	void DrawFPS(void);
+
+	LPDIRECT3D9				m_pD3D;			// Direct3Dオブジェクト
+	LPDIRECT3DDEVICE9		m_pD3DDevice;	// Deviceオブジェクト(描画に必要)
+	LPD3DXFONT				m_pFont;		// フォントへのポインタ
+};
+
+#endif
