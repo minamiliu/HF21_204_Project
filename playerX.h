2df2769ea04644310d978a7c70632d8d0e306a4f
Liu Nan-Hung <josong04@gmail.com>
2016-11-17 12:10:52 +0900

    add playerX.cpp

---
+++ playerX.h
@@ -0,0 +1,44 @@
+//============================================
+//
+// タイトル:	 CL25課題
+// プログラム名: playerX.h
+// 作成者:		 HAL東京ゲーム学科　劉南宏
+// 作成日:       2016/11/15
+//
+//============================================
+
+#ifndef _PLAYERX_H_
+#define _PLAYERX_H_
+
+//============================================
+//インクルードファイル
+//============================================
+#include "sceneX.h"
+
+//============================================
+//マクロ定義
+//============================================
+
+//============================================
+//構造体定義
+//============================================
+
+class CPlayerX : public CSceneX
+{
+public:
+
+	CPlayerX();
+	virtual ~CPlayerX();
+
+	HRESULT Init(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 scl, float speed);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	static CPlayerX *Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 scl, float speed);
+
+private:
+
+};
+
+#endif
